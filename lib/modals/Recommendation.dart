33aea0780d90966e3d3c6766ec567d000b0dc6c6
sanchitsaran <sanchitsaran1997@gmail.com>
2024-04-11 13:44:31 +0530

    first commit

---
+++ lib/modals/Recommendation.dart
@@ -0,0 +1,40 @@
+class Recommendation {
+  final String? name, source, text;
+
+  Recommendation({this.name, this.source, this.text});
+}
+
+final List<Recommendation> demo_recommendations = [
+
+  Recommendation(
+    name: "Flutter Developer",
+    source: "Torry Harris Integration Solutions",
+    text:
+    "• Development of Mobile applications using Flutter Framework(Dart).\n"
+        '• Translate designs to front-end code, estimate time and determine requirements and specifications.\n'
+        '• Collaborate with teams on the execution of ideas and projects.\n'
+        '• Excellent debugging, problem solving, optimization and testing skills.\n'
+        '•  Experience in working with APIGEE service provided via sequence diagram. \n'
+        ' • Contributed in improvement of the app performance as well as processes in the squad.\n'
+        '• Appreciated for proactiveness and my initiatives, and always been a team player.\n'
+  ),
+  Recommendation(
+    name: "Flutter Developer",
+    source: "The NineHertz Pvt. Ltd",
+    text:
+        "• Development of Mobile applications using Flutter Framework(Dart).\n"
+'• Translate designs to front-end code, estimate time and determine requirements and specifications.\n'
+'• Collaborate with teams on the execution of ideas and projects.\n'
+'• Excellent debugging, problem solving, optimization and testing skills.\n'
+'• Worked with 3rd party plugins: provider, getX, bLoc, http, Google Maps ,Rive animation, payment gateway(Razor Pay , Stripe) ,Firebase(FCM), Socket.\n',
+  ),
+  Recommendation(
+    name: "Android Developer",
+    source: "HawksCode Softwares Pvt. Ltd",
+    text:
+    "• Development of Mobile applications using Android Framework(JAVA), XML, JSON etc.\n"
+        '• Excellent debugging, problem solving, optimization and testing skills.\n'
+        '• Worked with 3rd party libraries : Retrofit 2.0 , Asynch http client ,\n Universal image loader , picasso , glide , Twilio (Video Calling), payment gateway(Razor Pay),\n PAYTM , Firebase.'
+         ),
+
+];
